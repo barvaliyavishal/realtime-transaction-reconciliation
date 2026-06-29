c0ec1a7aa59aedbb0153210aa2d1e385cd3cf7f7
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2026-06-29 10:02:58 -0400

    Phase 2: Spark Structured Streaming reads Kafka and parses JSON into typed columns

---
+++ dev/spark_stream.py
@@ -0,0 +1,29 @@
+import os
+os.environ["HADOOP_HOME"] = "C:\\hadoop"
+os.environ["PATH"] = os.environ["PATH"] + ";C:\\hadoop\\bin"
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+
+schema = "transaction_id STRING, card_id STRING, amount DOUBLE, currency STRING, merchant STRING, status STRING, event_time TIMESTAMP"
+
+spark = SparkSession. \
+        builder \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
+        .appName("kafka_receiver") \
+        .getOrCreate()
+
+df = spark.readStream.format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "transactions") \
+    .load()
+
+parsed_df = df.withColumn("parsed_value", from_json(col("value").cast("string"), schema)) \
+    .select("parsed_value.*") \
+    .writeStream \
+    .format("console") \
+    .option("truncate", "false") \
+    .start() \
+    .awaitTermination()
+    
