891808376abfb18d03b9138a9f5b7e2072297889
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2026-06-29 12:17:21 -0400

    Testing batch

---
+++ dev/batch_aggregations.py
@@ -0,0 +1,23 @@
+import os
+os.environ["HADOOP_HOME"] = "C:\\hadoop"
+os.environ["PATH"] = os.environ["PATH"] + ";C:\\hadoop\\bin"
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+
+schema = "transaction_id STRING, card_id STRING, amount DOUBLE, currency STRING, merchant STRING, status STRING, event_time TIMESTAMP"
+
+spark = SparkSession. \
+        builder \
+        .config("spark.jars.packages", "org.apache.iceberg:iceberg-aws-bundle:1.9.1,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.9.1") \
+        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+        .config("spark.sql.catalog.glue", "org.apache.iceberg.spark.SparkCatalog") \
+        .config("spark.sql.catalog.glue.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
+        .config("spark.sql.catalog.glue.warehouse", "s3://vishal-transaction-lake/warehouse") \
+        .config("spark.sql.catalog.glue.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+        .appName("kafka_receiver") \
+        .getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+
+spark.read.format("iceberg").load("glue.db.transactions_bronze").show()
